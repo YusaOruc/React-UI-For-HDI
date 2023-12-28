fbb6c9c2fe54a06829014c6833c20e54f669f94e
YusaOruc <h.yusa98@gmail.com>
2023-12-28 20:13:04 +0300

    feat: swagger template

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface ITest {
+  sayi:number
+}
+function Test(props:ITest) {
+  const{sayi}=props
+  return (
+    <p>
+   {sayi}
+  </p>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Test sayi={5}/>
     </div>
   );
 }
